9cd2dfaa1fe67d071071ca2f8a0f8a02897f1d6a
Juliano Candido <julianosgs@yahoo.com.br>
2022-05-20 08:05:52 -0300

    EcommerceAlimentos

---
+++ Projeto2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projeto2.MvcApplication" Language="C#" %>
---
+++ Projeto2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projeto2.Startup))]
+namespace Projeto2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
